db90683e115baff5dbfda3b231007e67f9b79d3d
Siva OS <syskantechnosoft@users.noreply.github.com>
2021-10-20 02:29:42 -0400

    Add files via upload

---
+++ eurekaserver/src/main/java/com/tcs/eurekaserver/EurekaserverApplication.java
@@ -0,0 +1,18 @@
+package com.tcs.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableSwagger2
+public class EurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverApplication.class, args);
+	}
+
+}
